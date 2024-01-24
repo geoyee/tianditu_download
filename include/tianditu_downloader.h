0aef045913d7cad7314d6bab0806de585efb1ac5
Yizhou Chen <geoyee@yeah.net>
2024-01-24 09:43:28 +0800

    feat: Update tianditu_downloader.h

--- include/tianditu_downloader.h
+++ include/tianditu_downloader.h
@@ -28,33 +28,57 @@ struct Extent
     double maxLat;
 };
 
+enum class TileMode
+{
+    ZYX,
+    ZXY
+};
+
 class TianDiTuDownloader
 {
 public:
-    TianDiTuDownloader() {}
+    TianDiTuDownloader() { }
+
     ~TianDiTuDownloader() = default;
 
-    const int startIndex() const { return _startIndex; }
-    const int threadNum() const { return _threadNum; }
-    const int level() const { return _level; }
-    const std::string &saveDir() const { return _saveDir; }
-    const std::vector<std::string> &keys() const { return _keys; }
-    const Extent &extent() const { return _extent; }
+    const int startIndex() const
+    {
+        return _startIndex;
+    }
 
-    bool loadConfig(const std::string &configPath);
+    const int level() const
+    {
+        return _level;
+    }
+
+    const std::string& saveDir() const
+    {
+        return _saveDir;
+    }
+
+    const std::vector<std::string>& keys() const
+    {
+        return _keys;
+    }
+
+    const Extent& extent() const
+    {
+        return _extent;
+    }
+
+    bool loadConfig(const std::string& configPath);
     static TileIndex lnglatToTileIndex(Lnglat lnglat, int level);
-    static bool downloadTile(TileIndex xy, int level, const std::string &key,
-                             const std::string &path);
+    static bool downloadTile(TileIndex xy, int level, const std::string& key, const std::string& path);
     void run();
 
 private:
     int _startIndex = 0;
-    int _threadNum = 1;
     int _level = 1;
     int _numOfTk = 10000;
     std::string _configPath = "./config.json";
     std::string _saveDir = "./";
     std::vector<std::string> _keys = {};
+    TileMode _tileMode = TileMode::ZYX;
     Extent _extent = {0, 0, 0, 0};
 };
 
