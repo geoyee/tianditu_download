3037cb4425134d8a70a5e9d0194f353854df86be
Yizhou Chen <geoyee@yeah.net>
2024-01-24 09:44:34 +0800

    feat: Update tianditu_downloader.cpp

--- src/tianditu_downloader.cpp
+++ src/tianditu_downloader.cpp
@@ -2,13 +2,13 @@
 #include <fstream>
 #include <filesystem>
 #include <cmath>
-#include "httplib.h"
 #include "nlohmann/json.hpp"
+#include "httplib.h"
 #include "tianditu_downloader.h"
 
 #define M_PI 3.14159265358979323846
 
-bool TianDiTuDownloader::loadConfig(const std::string &configPath)
+bool TianDiTuDownloader::loadConfig(const std::string& configPath)
 {
     _configPath = configPath;
     nlohmann::json jsonSolver;
@@ -19,7 +19,6 @@ bool TianDiTuDownloader::loadConfig(const std::string &configPath)
     try
     {
         _startIndex = jsonSolver.at("startIndex");
-        _threadNum = jsonSolver.at("threadNum"); // TODO: 这部分用起来
         _level = jsonSolver.at("level");
         _numOfTk = jsonSolver.at("numOfTk");
         _saveDir = jsonSolver.at("saveDir");
@@ -33,11 +32,13 @@ bool TianDiTuDownloader::loadConfig(const std::string &configPath)
         {
             _keys[i] = jsonSolver.at("keys").at(i);
         }
-        _extent = {jsonSolver.at("extent")[0], jsonSolver.at("extent")[1],
-                   jsonSolver.at("extent")[2], jsonSolver.at("extent")[3]};
+        _tileMode = jsonSolver.at("tileMode") == "ZYX" ? TileMode::ZYX : TileMode::ZXY;
+        _extent = {jsonSolver.at("extent")[0],
+                   jsonSolver.at("extent")[1],
+                   jsonSolver.at("extent")[2],
+                   jsonSolver.at("extent")[3]};
 
         LOG(INFO) << "Start Index:\t" << _startIndex;
-        LOG(INFO) << "Thread Num: \t" << _threadNum;
         LOG(INFO) << "Level:      \t" << _level;
         LOG(INFO) << "One Of TK:  \t" << _numOfTk;
         LOG(INFO) << "SaveDir:    \t" << _saveDir;
@@ -46,8 +47,9 @@ bool TianDiTuDownloader::loadConfig(const std::string &configPath)
         {
             LOG(INFO) << "            \t" << _keys.at(i);
         }
-        LOG(INFO) << "Extent: \t\t" << _extent.minLng << " " << _extent.maxLng
-                  << " " << _extent.minLat << " " << _extent.maxLat;
+        LOG(INFO) << "Tile Mode:  \t" << (_tileMode == TileMode::ZYX ? "ZYX" : "ZXY");
+        LOG(INFO) << "Extent:     \t" << _extent.minLng << " " << _extent.maxLng << " " << _extent.minLat << " "
+                  << _extent.maxLat;
         LOG(INFO) << "======== Load Success ========";
         return true;
     }
@@ -67,14 +69,11 @@ TileIndex TianDiTuDownloader::lnglatToTileIndex(Lnglat lnglat, int level)
     return TileIndex{static_cast<int>(titleX), static_cast<int>(titleY)};
 }
 
-bool TianDiTuDownloader::downloadTile(TileIndex xy, int level,
-                                      const std::string &key,
-                                      const std::string &path)
+bool TianDiTuDownloader::downloadTile(TileIndex xy, int level, const std::string& key, const std::string& path)
 {
     // 请求头
     httplib::Headers headers;
-    headers.insert(
-        {"Accept", "text/html,application/xhtml+xml,application/xml;"});
+    headers.insert({"Accept", "text/html,application/xhtml+xml,application/xml;"});
     headers.insert({"User-Agent", "Mozilla/5.0"});
 
     // 请求参数
@@ -101,8 +100,7 @@ bool TianDiTuDownloader::downloadTile(TileIndex xy, int level,
         std::ofstream out(path, std::ios::binary);
         out << res->body;
         out.close();
-        LOG(INFO) << "[SUCCESS] tile " << level << "_" << xy.y << "_" << xy.x
-                  << " saved";
+        LOG(INFO) << "[SUCCESS] tile " << level << "_" << xy.y << "_" << xy.x << " saved";
         return true;
     }
     else
@@ -134,16 +132,27 @@ void TianDiTuDownloader::run()
 
     for (int i = _startIndex; i < tileNum; ++i)
     {
-        TileIndex xy = {minXY.x + (i % (maxXY.x - minXY.x + 1)),
-                        minXY.y + (i / (maxXY.x - minXY.x + 1))};
+        TileIndex xy = {minXY.x + (i % (maxXY.x - minXY.x + 1)), minXY.y + (i / (maxXY.x - minXY.x + 1))};
 
-        // 按照Z（文件夹）/Y（文件夹）/X（文件夹）/tile.png的路径保存数据
-        std::string filePath = levelPath + "/" + std::to_string(xy.y);
-        if (!std::filesystem::exists(std::filesystem::path(filePath)))
+        std::string filePath = levelPath;
+        if (_tileMode == TileMode::ZYX)
         {
-            std::filesystem::create_directory(std::filesystem::path(filePath));
+            filePath = filePath + "/" + std::to_string(xy.y);
+            if (!std::filesystem::exists(std::filesystem::path(filePath)))
+            {
+                std::filesystem::create_directory(std::filesystem::path(filePath));
+            }
+            filePath = filePath + "/" + std::to_string(xy.x);
         }
-        filePath = filePath + "/" + std::to_string(xy.x);
+        else
+        {
+            filePath = filePath + "/" + std::to_string(xy.x);
+            if (!std::filesystem::exists(std::filesystem::path(filePath)))
+            {
+                std::filesystem::create_directory(std::filesystem::path(filePath));
+            }
+            filePath = filePath + "/" + std::to_string(xy.y);
+        }
         std::filesystem::create_directory(std::filesystem::path(filePath));
         filePath = filePath + "/" + "tile.png";
         TianDiTuDownloader::downloadTile(xy, _level, key, filePath);
