824e20d18900d1360e87d027dc303f027977a5ef
Yizhou Chen <geoyee@yeah.net>
2024-01-29 18:48:46 +0800

    docs: Update note

--- main.cpp
+++ main.cpp
@@ -4,19 +4,30 @@ INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
+    if (argc != 2)
     {
-        std::cout << "please input config path" << std::endl;
+        std::cout << "Invalid argument of " << argv[0] << "\n";
+        std::cout << "Run " << argv[0] << " -h for help" << std::endl;
         return -1;
     }
-
-    TianDiTuDownloader tdtLoader;
-    if (!tdtLoader.loadConfig(argv[1]))
+    else
     {
-        return -1;
+        if (strcmp(argv[1], "-h") == 0)
+        {
+            std::cout << "Usage: " << argv[0] << " <configPath>\n";
+            std::cout << "\t configPath: Path of config file.\n";
+            std::cout << "Example: " << argv[0] << " config.json" << std::endl;
+            return 0;
+        }
+        else
+        {
+            TianDiTuDownloader tdtLoader;
+            if (!tdtLoader.loadConfig(argv[1]))
+            {
+                return -1;
+            }
+            tdtLoader.run();
+        }
     }
-
-    tdtLoader.run();
-
     return 0;
 }
